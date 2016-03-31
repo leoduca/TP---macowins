a0285d17d4082c6fac8c7fcf783f6c3a1b351a9e
leoduca <leoduca86@hotmail.com>
2016-03-31 18:15:49 -0300

    Modificaciones Test Ganancia

--- Macowins/src/test/java/MainTest.java
+++ Macowins/src/test/java/MainTest.java
@@ -4,29 +4,17 @@ import org.junit.*;
 
 public class MainTest {	
 	
-	
-	Pantalon levis = new Pantalon(true);
-	
-	Camisa argenta = new Camisa(false);
-	Camisa tomy = new Camisa(true);
-	
-	Saco sacoNacional = new Saco(false);
-	Saco sacoImportado = new Saco(true);	
-
 
 	@Test
 	public void Ganancias(){
 		
 		LocalDate hoy = LocalDate.now();
 		Pantalon argentino = new Pantalon(false);
-		Venta venta1 = new Venta(argentino, 2,hoy); 
+		Venta venta1 = new Venta(argentino, 1,hoy); 
 		
 		Local macowins = new Local();	
 		
 		macowins.addVenta(venta1);		
-		double ganancia = macowins.gananciasDelDia(hoy);
-		
-		System.out.println(ganancia);
 		
 		Assert.assertEquals(350, macowins.gananciasDelDia(hoy),1);
 	}	
